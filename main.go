d7449cb813e77efbd442f5faaed8f9ef1910dff0
mrDock21 <danes_warrior@hotmail.com>
2020-09-29 11:54:41 -0500

    Arreglado el bug

--- main.go
+++ main.go
@@ -17,10 +17,9 @@ func psedoTernary(expression bool, trueRes string, falseRes string) string {
 func main() {
 	var base, height int = 1, 1
 	// Read from console
-	num_args, err1 := fmt.Scanf("%d", &base)
-	_, err2 := fmt.Scanf("%d", &height)
+	_, err1 := fmt.Scanf("%d\n%d", &base, &height)
 
-	if err1 == nil && err2 == nil {
+	if err1 == nil {
 		fmt.Printf("%f\n", float32(base)*float32(height)/2.0)
 	}
 }
