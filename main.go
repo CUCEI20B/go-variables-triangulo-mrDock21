5ac0ae668f1297aca799f22155b6c183e3a6ef9f
mrDock21 <danes_warrior@hotmail.com>
2020-09-29 11:51:05 -0500

    Agregada area del triangulo

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func psedoTernary(expression bool, trueRes string, falseRes string) string {
+	res := ""
+	if expression {
+		res = trueRes
+	} else {
+		res = falseRes
+	}
+	return res
+}
+
+func main() {
+	var base, height int = 1, 1
+	// Read from console
+	num_args, err1 := fmt.Scanf("%d", &base)
+	_, err2 := fmt.Scanf("%d", &height)
+
+	if err1 == nil && err2 == nil {
+		fmt.Printf("%f\n", float32(base)*float32(height)/2.0)
+	}
 }
